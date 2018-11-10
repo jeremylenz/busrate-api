6133e4fb50856a1cb6c1c0a912aa9c53e7280361
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-10 16:19:26 -0500

    use fast_inserter

--- app/models/historical_departure.rb
+++ app/models/historical_departure.rb
@@ -12,7 +12,7 @@ class HistoricalDeparture < ApplicationRecord
 
   def self.extract_vehicle_positions(response)
     timestamp = response['Siri']['ServiceDelivery']['ResponseTimestamp']
-    return {} unless response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0].present?
+    return [] unless response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0].present?
     vehicle_activity = response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
     new_vehicle_position_params = vehicle_activity.map do |data|
       next unless data['MonitoredVehicleJourney'].present?
@@ -26,7 +26,6 @@ class HistoricalDeparture < ApplicationRecord
       vehicle = Vehicle.find_or_create_by(vehicle_ref: vehicle_ref)
       bus_line = BusLine.find_by(line_ref: line_ref)
       next unless vehicle.present? && bus_line.present?
-
       {
           vehicle_id: vehicle.id,
           bus_line_id: bus_line.id,
@@ -35,10 +34,10 @@ class HistoricalDeparture < ApplicationRecord
           arrival_text: arrival_text,
           feet_from_stop: feet_from_stop,
           stop_ref: stop_ref,
-          timestamp: timestamp,
+          timestamp: Time.rfc3339(timestamp),
       }
     end.compact
-    return {} if new_vehicle_position_params.empty?
+    return [] if new_vehicle_position_params.empty?
 
     fast_inserter_variable_columns = new_vehicle_position_params.first.keys.map(&:to_s)
     fast_inserter_values = new_vehicle_position_params.map { |nvpp| nvpp.values }
@@ -53,7 +52,9 @@ class HistoricalDeparture < ApplicationRecord
       values: fast_inserter_values,
     }
     inserter = FastInserter::Base.new(fast_inserter_params)
+    last_id = VehiclePosition.order(id: :desc).first.id
     inserter.fast_insert
+    VehiclePosition.where(['id > ?', last_id])
   end
 
   def self.scrape_departures(old_vehicle_positions, new_vehicle_positions)
@@ -66,20 +67,33 @@ class HistoricalDeparture < ApplicationRecord
       end
       next unless next_position.present?
       if is_departure?(old_vehicle_position, next_position)
-        comparison = {
-          old: old_vehicle_position,
-          new: next_position,
-          departed_at: next_position[:timestamp]
+        new_departure = {
+          stop_ref: next_position.stop_ref,
+          line_ref: next_position.line_ref,
+          vehicle_ref: next_position.vehicle_ref,
+          departure_time: next_position.timestamp,
         }
-        departures << comparison
-        HistoricalDeparture.create(
-          stop_ref: comparison[:new].stop_ref,
-          line_ref: comparison[:new].line_ref,
-          vehicle_ref: comparison[:new].vehicle_ref,
-          departure_time: comparison[:departed_at]
-        )
+        departures << new_departure
       end
     end
+
+    fast_inserter_variable_columns = ['stop_ref', 'line_ref', 'vehicle_ref', 'departure_time']
+    fast_inserter_values = departures.map do |dep|
+      [dep[:stop_ref], dep[:line_ref], dep[:vehicle_ref], dep[:departure_time]]
+    end
+    fast_inserter_params = {
+      table: 'historical_departures',
+      static_columns: {},
+      options: {
+        timestamps: true,
+        group_size: 2_000,
+      },
+      variable_columns: fast_inserter_variable_columns,
+      values: fast_inserter_values,
+    }
+    inserter = FastInserter::Base.new(fast_inserter_params)
+    inserter.fast_insert
+
     departures
 
   end
@@ -138,21 +152,8 @@ class HistoricalDeparture < ApplicationRecord
 
   def self.grab_all
     existing_count = HistoricalDeparture.all.count
-    puts "round 1"
     response = HTTParty.get(ApplicationController::ALL_VEHICLES_URL)
-    # byebug
     vehicle_positions_a = extract_vehicle_positions(response)
-    puts "waiting"
-    sleep(33)
-    puts "round 2"
-    response = HTTParty.get(ApplicationController::ALL_VEHICLES_URL)
-    vehicle_positions_b = extract_vehicle_positions(response)
-
-    puts "extracting"
-    scrape_departures(vehicle_positions_a, vehicle_positions_b)
-
-    new_count = HistoricalDeparture.all.count - existing_count
-    puts "#{new_count} historical departures created."
   end
 
   def self.grab_all_smart
@@ -191,9 +192,10 @@ class HistoricalDeparture < ApplicationRecord
 
     stale_vehicle_positions = VehiclePosition.at_stop.older_than(30).newer_than(300)
     vehicles_to_check = stale_vehicle_positions.map { |vp| vp.vehicle }
-    vehicle_positions_to_check = vehicles_to_check.map { |vehicle| vehicle.latest_position }
+    vehicle_positions_to_check = vehicles_to_check.map { |vehicle| vehicle.latest_position }.compact
     new_vehicle_positions = []
     vehicle_positions_to_check.each do |vp|
+      next if vp.blank?
       url_addon = ERB::Util.url_encode(vp.vehicle_ref)
       url = ApplicationController::LIST_OF_VEHICLES_URL + "&VehicleRef=" + url_addon
       response = HTTParty.get(url)
