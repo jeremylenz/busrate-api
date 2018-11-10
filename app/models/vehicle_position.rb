3a0c7adc6eeab5828ab9676a5c4c44afd0dd01f5
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-10 14:03:52 -0500

    add VehiclePosition scopes

--- app/models/vehicle_position.rb
+++ app/models/vehicle_position.rb
@@ -2,5 +2,8 @@ class VehiclePosition < ApplicationRecord
 
   belongs_to :vehicle
   belongs_to :bus_line
+
+  scope :at_stop, -> { where(arrival_text: "at stop") }
+  scope :older_than, -> (num) { where(["timestamp < ?", num.seconds.ago]) }
   
 end
