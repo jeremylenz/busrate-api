e12f9089e52942c9828130e13b59cb56898874b4
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-13 10:26:39 -0500

    camelCase URL params

--- app/controllers/api/v1/historical_departures_controller.rb
+++ app/controllers/api/v1/historical_departures_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::HistoricalDeparturesController < ApplicationController
 
   def index
-    line_ref = BusLine.find_by(line_ref: params[:line_ref])&.line_ref
+    line_ref = BusLine.find_by(line_ref: params[:lineRef])&.line_ref
     stop_ref = BusStop.find_by(stop_ref: params[:bus_stop_id])&.stop_ref
 
-    if params[:bus_stop_id].blank? || params[:line_ref].blank?
-      render json: {error: "You must specify both a stop_ref and a line_ref"}, status: 422
+    if params[:bus_stop_id].blank? || params[:lineRef].blank?
+      render json: {error: "You must specify both a stop ref and a lineRef"}, status: 422
       return
     elsif stop_ref.blank?
       render json: {error: "stop_ref #{params[:bus_stop_id]} not found"}, status: 422
--- app/controllers/api/v1/mta_bus_routes_controller.rb
+++ app/controllers/api/v1/mta_bus_routes_controller.rb
@@ -31,8 +31,6 @@ class Api::V1::MtaBusRoutesController < ApplicationController
     list_of_stops = "#{route_id}.json?key=#{MTA_BUS_API_KEY}&includePolylines=false&version=2"
     list_of_stops = ERB::Util.url_encode(list_of_stops)
     list_of_stops = base_url + list_of_stops
-    puts list_of_stops
-    # byebug
     response = HTTParty.get(list_of_stops)
 
     if response.code == 200
