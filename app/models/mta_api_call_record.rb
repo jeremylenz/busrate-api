43ce9347171af65713293315358dc5f486d6c8a9
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-13 20:17:50 -0500

    pessimistic locking

--- app/models/mta_api_call_record.rb
+++ app/models/mta_api_call_record.rb
@@ -1,8 +1,9 @@
 class MtaApiCallRecord < ApplicationRecord
 
   def self.most_recent
-    self.limit(10).each(&:reload)
-    self.order(created_at: :desc).limit(1).reload.first
+    MtaApiCallRecord.transaction do
+      self.lock.order(created_at: :desc).limit(1).reload.first
+    end
   end
 
 end
