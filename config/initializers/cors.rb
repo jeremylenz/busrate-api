6039c06dc68d0e5b1eb0d1a29fe808e912fc8a45
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-21 09:25:40 -0500

    add droplet to Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'http://10.0.1.107:3000'
 
     resource '*',
       headers: :any,
