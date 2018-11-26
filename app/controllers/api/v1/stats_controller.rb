78cf68b8cf1109af24a8e6144f10297dfbb3d2fe
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-26 15:20:12 -0500

    reorder response

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -13,20 +13,20 @@ class Api::V1::StatsController < ApplicationController
     @vehicle_count = Vehicle.all.count
 
     if @mta_api_call_records_count > 0
-      @avg_vehicles_per_api_call = @vehicle_position_recent_count / @mta_api_call_records_count
+      @avg_vehicle_positions_per_api_call = @vehicle_position_recent_count / @mta_api_call_records_count
       @avg_departures_per_api_call = @historical_departure_recent_count / @mta_api_call_records_count
     end
 
     response = {
       mta_api_all_vehicles_calls: @mta_api_call_records_count,
-      bus_lines: @bus_line_count,
-      bus_stops: @bus_stop_count,
       historical_departures: number_with_delimiter(@historical_departure_count, delimiter: ','),
       historical_departures_last_300_seconds: @historical_departure_recent_count,
       vehicle_positions: @vehicle_position_count,
       vehicle_positions_last_300_seconds: @vehicle_position_recent_count,
+      bus_lines: @bus_line_count,
+      bus_stops: @bus_stop_count,
       vehicles: @vehicle_count,
-      avg_vehicles_per_api_call: @avg_vehicles_per_api_call,
+      avg_vehicle_positions_per_api_call: @avg_vehicle_positions_per_api_call,
       avg_departures_per_api_call: @avg_departures_per_api_call,
       response_timestamp: Time.zone.now.in_time_zone("EST"),
     }
