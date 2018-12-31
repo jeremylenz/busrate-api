49d6244187a9d41bcc0fce2238b564d0fe5346d0
Jeremy Lenz <jeremylenz@gmail.com>
2018-12-31 16:06:53 -0500

    testing cursor gem

--- app/models/historical_departure.rb
+++ app/models/historical_departure.rb
@@ -233,62 +233,75 @@ class HistoricalDeparture < ApplicationRecord
     false
   end
 
-  def self.doit(age_in_secs, skip_non_nils = true)
+  def self.doit(age_in_secs)
     hds = HistoricalDeparture.newer_than(age_in_secs)
-    HistoricalDeparture.calculate_headways(hds, skip_non_nils)
+    HistoricalDeparture.calculate_headways(hds)
   end
 
-  def self.calculate_headways(unsorted_historical_departures, skip_non_nils = true)
-    return if unsorted_historical_departures.blank? || unsorted_historical_departures.length < 2
-    deps = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC").limit(20_000) # make sure it's sorted
-    process_headways(deps, skip_non_nils)
-  end
+  def self.calculate_headways(unsorted_historical_departures)
+    return if unsorted_historical_departures.blank? || unsorted_historical_departures.count < 2
+    HistoricalDeparture.transaction do
+      length = unsorted_historical_departures.count
+      cursor = unsorted_historical_departures.lock.order("stop_ref, line_ref, departure_time DESC").each_instance
+      lookahead = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC").offset(1).each_instance
 
-  def self.process_headways(sorted_deps, skip_non_nils = true)
-    return if sorted_deps.blank? || sorted_deps.length < 2
-    start_time = Time.current
-    last_index = sorted_deps.count - 1
-    logger.info "Calculating #{last_index} headways"
-    successful_count = 0
-    failure_count = 0
-    error_count = 0
-    non_nils_skipped = 0
-    sorted_deps.each_with_index do |current_departure, idx|
-      next if idx == last_index
-      if skip_non_nils && current_departure.headway.present?
-        # skip departures that already have a value for headway
-        non_nils_skipped += 1
-        next # thank u
-      end
-      previous_departure = sorted_deps[idx + 1]
-      unless current_departure.stop_ref == previous_departure.stop_ref && current_departure.line_ref == previous_departure.line_ref
-        failure_count += 1
-        print "failure_count: #{failure_count}        \r"
-        next
-      end
-      prev_id = previous_departure.id
-      headway = (current_departure.departure_time - previous_departure.departure_time).round.to_i
-      headway = nil if headway == 0
-      current_departure.update(
-        headway: headway,
-        previous_departure_id: prev_id,
-      )
+      puts cursor.fetch
+      puts lookahead.fetch
 
-      if current_departure.errors.any?
-        logger.info "Problem updating departure #{current_departure.id}: #{current_departure.errors.full_messages.join("; ")}"
-        error_count += 1
-      else
-        successful_count += 1
-        print "successful_count: #{successful_count}\r"
-      end
     end
-    logger.info "Updated #{successful_count} headways."
-    logger.info "Skipped #{failure_count} headways due to stop_ref/line_ref mismatch"
-    logger.info "Skipped #{non_nils_skipped} headways that were already present"
-    logger.info "Update failed for #{error_count} headways"
-    logger.info "Total #{successful_count + failure_count + error_count}"
-    logger.info "process_headways done after #{Time.current - start_time} seconds"
-
   end
+
+  # def self.calculate_headways(unsorted_historical_departures, skip_non_nils = true)
+  #   return if unsorted_historical_departures.blank? || unsorted_historical_departures.length < 2
+  #   deps = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC") # make sure it's sorted
+  #   process_headways(deps, skip_non_nils)
+  # end
+  #
+  # def self.process_headways(sorted_deps, skip_non_nils = true)
+  #   return if sorted_deps.blank? || sorted_deps.length < 2
+  #   start_time = Time.current
+  #   last_index = sorted_deps.count - 1
+  #   logger.info "Calculating #{last_index} headways"
+  #   successful_count = 0
+  #   failure_count = 0
+  #   error_count = 0
+  #   non_nils_skipped = 0
+  #   sorted_deps.each_with_index do |current_departure, idx|
+  #     next if idx == last_index
+  #     if skip_non_nils && current_departure.headway.present?
+  #       # skip departures that already have a value for headway
+  #       non_nils_skipped += 1
+  #       next # thank u
+  #     end
+  #     previous_departure = sorted_deps[idx + 1]
+  #     unless current_departure.stop_ref == previous_departure.stop_ref && current_departure.line_ref == previous_departure.line_ref
+  #       failure_count += 1
+  #       print "failure_count: #{failure_count}        \r"
+  #       next
+  #     end
+  #     prev_id = previous_departure.id
+  #     headway = (current_departure.departure_time - previous_departure.departure_time).round.to_i
+  #     headway = nil if headway == 0
+  #     current_departure.update(
+  #       headway: headway,
+  #       previous_departure_id: prev_id,
+  #     )
+  #
+  #     if current_departure.errors.any?
+  #       logger.info "Problem updating departure #{current_departure.id}: #{current_departure.errors.full_messages.join("; ")}"
+  #       error_count += 1
+  #     else
+  #       successful_count += 1
+  #       print "successful_count: #{successful_count}\r"
+  #     end
+  #   end
+  #   logger.info "Updated #{successful_count} headways."
+  #   logger.info "Skipped #{failure_count} headways due to stop_ref/line_ref mismatch"
+  #   logger.info "Skipped #{non_nils_skipped} headways that were already present"
+  #   logger.info "Update failed for #{error_count} headways"
+  #   logger.info "Total #{successful_count + failure_count + error_count}"
+  #   logger.info "process_headways done after #{Time.current - start_time} seconds"
+  #
+  # end
 
 end
