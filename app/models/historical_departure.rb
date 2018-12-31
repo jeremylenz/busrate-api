2401a83fd5908f461879891cfb45a9d6f951a0d4
Jeremy Lenz <jeremylenz@gmail.com>
2018-12-31 16:30:35 -0500

    wip

--- app/models/historical_departure.rb
+++ app/models/historical_departure.rb
@@ -244,7 +244,7 @@ class HistoricalDeparture < ApplicationRecord
     HistoricalDeparture.transaction do
       length = unsorted_historical_departures.count
       lookahead = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC").offset(1).each_instance
-      cursor = unsorted_historical_departures.lock.order("stop_ref, line_ref, departure_time DESC").each_instance do |hd|
+      cursor = unsorted_historical_departures.lock.order("stop_ref, line_ref, departure_time DESC").each_instance(symbolize_keys: true) do |hd|
         hd_hash = {
         :id => hd.id,
         :headway => hd.headway,
