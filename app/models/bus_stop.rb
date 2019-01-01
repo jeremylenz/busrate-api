ad2ebe1bd8c5abcc577e4fdacb86d5e619850228
Jeremy Lenz <jeremylenz@gmail.com>
2019-01-01 11:22:27 -0500

    bus stop clean_up

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -5,4 +5,39 @@ class BusStop < ApplicationRecord
   validates_presence_of :stop_ref
   validates_uniqueness_of :stop_ref
 
+  def clean_up(limit)
+    start_time = Time.current
+    logger.info "Cleaning up bus stops..."
+    bad_bus_stop = self.where(stop_ref: nil).first
+    ids_to_purge = []
+    successful_count = 0
+    purge_count = 0
+
+    hds = bad_bus_stop.historical_departures.limit(limit).each_instance do |hd|
+      if hd.stop_ref.blank?
+        purge_count += 1
+        ids_to_purge << hd.id
+      end
+      real_bus_stop = BusStop.find_by(stop_ref: hd.stop_ref)
+      if real_bus_stop.blank?
+        hd.errors << "Couldn't find bus stop with stop_ref #{hd.stop_ref}"
+      end
+      logger.info "#{[hd.id, hd.stop_ref, hd.bus_stop_id]} --> #{[hd.id, real_bus_stop.stop_ref, real_bus_stop.id]}"
+      hd.update(
+        bus_stop_id: real_bus_stop.id
+      )
+      if hd.errors.any?
+        error_count += 1
+        logger.info "Error updating historical departure #{hd.id} - #{hd.errors.full_messages.join("; ")}"
+      else
+        successful_count += 1
+      end
+    end # of each_instance
+
+    logger.info "#{purge_count} departures purged" if purge_count > 0
+    logger.info "#{successful_count} departures moved"
+    logger.info "#{bad_bus_stop.historical_departures.count} departures remaining"
+    logger.info "clean_up complete in #{(Time.current - start_time).round(2)} seconds"
+  end
+
 end
