e2e4aeba2e8874c459817133c1e5ef071a38415b
Jeremy Lenz <jeremylenz@gmail.com>
2019-01-01 11:03:12 -0500

    add presence validations for stop_ref and line_ref

--- app/models/bus_line.rb
+++ app/models/bus_line.rb
@@ -1,7 +1,7 @@
 class BusLine < ApplicationRecord
 
   has_many :vehicle_positions
-
+  validates_presence_of :line_ref
   validates_uniqueness_of :line_ref
 
 
--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -2,6 +2,7 @@ class BusStop < ApplicationRecord
 
   has_many :vehicle_positions
   has_many :historical_departures
+  validates_presence_of :stop_ref
   validates_uniqueness_of :stop_ref
 
 end
