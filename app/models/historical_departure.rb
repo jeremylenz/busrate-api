ec63c78a5f3c1bce05a7b373e70293973cc39bb3
Jeremy Lenz <jeremylenz@gmail.com>
2018-12-31 22:13:45 -0500

    batch size experimenting

--- app/models/historical_departure.rb
+++ app/models/historical_departure.rb
@@ -257,7 +257,7 @@ class HistoricalDeparture < ApplicationRecord
       current_batch_stop_ref = nil
       current_batch_line_ref = nil
 
-      cursor = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC").each_instance(block_size: 1000) do |current_departure|
+      cursor = unsorted_historical_departures.order("stop_ref, line_ref, departure_time DESC").each_instance(block_size: 500) do |current_departure|
         if current_batch_stop_ref.blank?
           # we are at the beginning of a new batch
           current_batch_stop_ref = current_departure.stop_ref
