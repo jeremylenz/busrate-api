400352504889ae2f0c106f25ebd08c19fa15a58b
Jeremy Lenz <jeremylenz@gmail.com>
2019-02-08 22:42:10 -0500

    method to extract stop list from JSON response

--- app/models/bus_line.rb
+++ app/models/bus_line.rb
@@ -4,5 +4,20 @@ class BusLine < ApplicationRecord
   validates_presence_of :line_ref
   validates_uniqueness_of :line_ref
 
+  def ordered_stop_refs
+    return nil if self.response.nil?
+
+    response = JSON.parse(self.response)
+    stop_groups_data = response['data']['entry']['stopGroupings'][0]['stopGroups']
+
+    stop_groups_data.map do |stop_group|
+      destination_name = stop_group['name']['name']
+      stop_refs = stop_group['stopIds']
+      {
+        destination_name: destination_name,
+        stop_refs: stop_refs,
+      }
+    end
+  end
 
 end
