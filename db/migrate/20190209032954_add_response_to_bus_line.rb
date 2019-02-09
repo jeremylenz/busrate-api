4282200957a568749a1b3983b40c2351bb2f862e
Jeremy Lenz <jeremylenz@gmail.com>
2019-02-08 22:48:40 -0500

    change field name

--- db/migrate/20190209032954_add_response_to_bus_line.rb
+++ db/migrate/20190209032954_add_response_to_bus_line.rb
@@ -1,5 +1,5 @@
 class AddResponseToBusLine < ActiveRecord::Migration[5.2]
   def change
-    add_column :bus_lines, :response, :json
+    add_column :bus_lines, :stop_refs_response, :json
   end
 end
