a434dd041e2d0e111adcab7440236f7c653a1b27
Jeremy Lenz <jeremylenz@gmail.com>
2019-02-18 17:15:40 -0500

    Update schedule.example.rb to reflect real one

--- config/schedule.example.rb
+++ config/schedule.example.rb
@@ -30,24 +30,51 @@ every 8.minutes do
 end
 
 every 1.minute do
-  runner "HistoricalDeparture.grab_and_go"
+ runner "HistoricalDeparture.grab_all"
+end
+
+every 1.minute do
+  runner "HistoricalDeparture.wait_and_grab(32)"
+end
+
+every 1.minute do
+  runner "HistoricalDeparture.wait_and_grab(15)"
+end
+
+every 1.minute do
+  runner "HistoricalDeparture.wait_and_grab(45)"
 end
 
 every 1.minute do
   runner "HistoricalDeparture.scrape_all"
 end
 
-every 2.minutes do
-  runner "HistoricalDeparture.calculate_headways(HistoricalDeparture.newer_than(14_400))"
+# every 2.minutes do
+#   runner "HistoricalDeparture.calculate_headways(HistoricalDeparture.newer_than(14_400))"
+# end
+
+every 30.minutes do
+  runner "HistoricalDeparture.chunk_headways"
 end
 
-every 29.minutes do
-  command "curl localhost:3000/api/v1/ping"
+# skip 0 and 30 minutes for this one so as not to conflict with chunk_headways
+every '6,12,18,24,36,42,48,54 * * * *' do
+  runner "HistoricalDeparture.calculate_headways(HistoricalDeparture.newer_than(3_600))"
 end
 
-every 1.hour do
-  runner "HistoricalDeparture.purge_duplicates_newer_than(3700)"
+# same for this one
+every '3,9,15,21,27,39,45,51,57 * * * *' do
+  runner "HistoricalDeparture.interpolate_recent(3_600)"
 end
+
+# every 29.minutes do
+#   command "curl localhost:3000/api/v1/ping"
+# end
+
+# this one takes too much memory and gets the process killed
+# every 1.hour do
+#   runner "HistoricalDeparture.purge_duplicates_newer_than(3700)"
+# end
 
 every 1.hour do
   runner "HistoricalDeparture.update_count"
