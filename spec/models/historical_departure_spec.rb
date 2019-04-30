5233c72be564830f484da0e8e74a95e9d5247839
Jeremy Lenz <jeremylenz@gmail.com>
2019-04-29 21:02:24 -0400

    add basic tests

--- spec/models/historical_departure_spec.rb
+++ spec/models/historical_departure_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe HistoricalDeparture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:bus_stop) { BusStop.new }
+  let(:subject) {
+    HistoricalDeparture.new(
+      headway: nil,
+      bus_stop: bus_stop,
+    )
+  }
+  describe 'validations:' do
+    it 'is valid with valid properties' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with an invalid headway' do
+      subject.headway = 0
+      expect(subject).not_to be_valid
+    end
+  end
 end
